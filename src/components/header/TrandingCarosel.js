b04e79b868df06b81134de480455a9bf15a1b36a
Rana16468 <amsohelrana.me@gmail.com>
2024-02-27 09:57:23 +0600

    added pagination

--- src/components/header/TrandingCarosel.js
+++ src/components/header/TrandingCarosel.js
@@ -1,22 +1,32 @@
 import React, { useEffect, useState } from "react";
 
 import toast from "react-hot-toast";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const TrandingCarosel = () => {
   const [allProduct, setAllProduct] = useState([]);
   const [isLoading, setLoading] = useState(true);
   const [isError, setError] = useState("");
+  const navigate = useNavigate();
+  // pagination
+  const [page, setPage] = useState(0);
+  const [size, setSize] = useState(10);
+  const pages = Math.ceil(75 / size);
+
   useEffect(() => {
-    fetch("http://localhost:3013/api/v1/all_product", {
-      method: "GET",
-      headers: {
-        authorization: localStorage.getItem("token"),
-      },
-    })
+    fetch(
+      `http://localhost:3013/api/v1/all_product?page=${page}&limit=${size}`,
+      {
+        method: "GET",
+        headers: {
+          authorization: localStorage.getItem("token"),
+        },
+      }
+    )
       .then((res) => res.json())
       .then((data) => {
-        if (data.status === 401) {
+        if (data?.status === 401) {
+          navigate("/login");
           toast.success("Login First Then Access Then See The Product");
         }
         setLoading(false);
@@ -26,7 +36,7 @@ const TrandingCarosel = () => {
       .catch((error) => {
         setError(error);
       });
-  }, []);
+  }, [navigate, page, size]);
 
   return (
     <>
@@ -38,7 +48,58 @@ const TrandingCarosel = () => {
         Tranding Product
       </h1>
 
-      {allProduct.map((item, index) => (
+      <div className="flex justify-end">
+        {/* <div className="flex items-center justify-center">
+          <select className="rounded-sm h-12">
+            <option disabled selected>
+              Caragories
+            </option>
+            <option value="Internship">Internship</option>
+            <option value="Fresher">Fresher</option>
+            <option value="Semi-Experiences">Semi-Experiences</option>
+            <option value="Experiences">Experiences</option>
+            <option value="">All Jobs</option>
+          </select>
+          <input
+            type="search"
+            id="default-search"
+            className="block w-96 p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-r-lg  focus:ring-blue-500 focus:border-blue-500 bg-[#082f49] dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Find Your Job / Srach Your Job Name"
+            required
+          />
+        </div> */}
+        <div
+          style={{
+            marginLeft: "200px",
+            marginBottom: "50px",
+          }}>
+          <p>
+            Current Page : {page + 1} and Size:{size}
+          </p>
+          {[...Array(pages).keys()].map((number) => (
+            <button
+              className="mr-3 text-xl btn btn-outline btn-sm"
+              key={number}
+              onClick={() => {
+                setPage(number);
+              }}>
+              {number + 1}
+            </button>
+          ))}
+          <select
+            className="rounded-full btn btn-outline btn-sm"
+            onChange={(event) => setSize(event.target.value)}>
+            <option value="10" selected>
+              10
+            </option>
+            <option value="15">15</option>
+            <option value="20">20</option>
+            <option value="25">25</option>
+          </select>
+        </div>
+      </div>
+
+      {allProduct?.map((item, index) => (
         <div key={index}>
           <div className="grid lg:grid-cols-5 md:grid-cols-3 gap-2 sm:grid-cols-1 m-3">
             {item?.products?.map((v, index) => {
--- src/router/router.js
+++ src/router/router.js
@@ -5,6 +5,7 @@ import ADVTime from "../components/header/ADVTime";
 import Register from "../components/page/register/Register";
 import Login from "../components/page/register/Login";
 import Dashboard from "../components/page/BuyerDashboard/Dashboard";
+import PrivateRouter from "./PrivateRouter";
 
 const router = createBrowserRouter([
   {
@@ -18,7 +19,14 @@ const router = createBrowserRouter([
       { path: "/add_to_card", element: "" },
       { path: "/New_arrivals", element: "" },
       { path: "/trandint_product/:id", element: "" },
-      { path: "/buyer_dashboard/:id", element: <Dashboard /> },
+      {
+        path: "/buyer_dashboard/:id",
+        element: (
+          <PrivateRouter>
+            <Dashboard />
+          </PrivateRouter>
+        ),
+      },
     ],
   },
 ]);
